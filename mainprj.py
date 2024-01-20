2862cf080bfdda1abec009f8a6a2ce84caa8ee91
SyntaxPratyush <157193581+SyntaxPratyush@users.noreply.github.com>
2024-01-20 13:04:35 +0530

    Button Added

--- mainprj.py
+++ mainprj.py
@@ -13,8 +13,12 @@ def home_page():
     home.configure(bg="#1D3C37")
     
     headingimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Heading.png")
-    headinglabel = Label(home, image=headingimage)
+    headinglabel = Label(home, image=headingimage, highlightthickness=0, borderwidth=0)
     headinglabel.grid(row=0, column=0, columnspan=4)
+
+    homeimg = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Homebtn.png")
+    homebtn = Button(home, image=homeimg, highlightthickness=0, borderwidth=0)
+    homebtn.grid(row=0, column=1, columnspan=1)
 
     home.mainloop()
 
