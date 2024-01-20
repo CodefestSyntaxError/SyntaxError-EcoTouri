b7d37e6a701fdba30df3fd27f11048b58d12bfbe
Anonymous-alph <97744086+Anonymous-alph@users.noreply.github.com>
2024-01-20 16:20:37 +0530

    Added specifications and Comments

--- llmprj.py.py
+++ llmprj.py.py
@@ -7,16 +7,21 @@ model_path = "C:\\Users\\admin\\Desktop\\Codefest\\mistral-7b-instruct-v0.2.Q6_K
 # model variable
 model = Llama(model_path=model_path)
 
+#training message and user input message for generating information
 training_message = "You are a factual and reliable assistant that provides true information and text on various Eco-sustainable hotels, restraunts, best modes of sustainable transortation, various places to see and observe in the area and also the carbon footprint that has been emitted through this entire tourist place vacation plan. "
-user_input_message = "Generate a plan for a trip to a place consisting of Eco-sustainable hotels in the area, best restraunts, best cheap and eco-friendly modes of transport, best places to see in the area and also after the creation of a plan then tell the overall cost and approximate carbon footprint being emitted through this entire trip" 
+user_input_message = "Switzerland: Generate a plan for a trip to a place consisting of Eco-sustainable hotels in the area, best restraunts, best cheap and eco-friendly modes of transport, best places to see in the area and also after the creation of a plan then tell the overall cost and approximate carbon footprint being emitted through this entire trip" 
 
+#Initiates the llm to generate the information
 prompt = f"""<s>[INST] <<SYS>>
 {training_message}
 <</SYS>>
 {user_input_message} [/INST]"""
 
+#Specifies the max tokens
 max_tokens = 100
 
+#Generates the information
 output = model(prompt, max_tokens=max_tokens, echo=True)
 
+#printing the output
 print(output)
