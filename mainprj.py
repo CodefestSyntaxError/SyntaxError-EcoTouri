49a7c5405ed152b17b4ed6e141c5cf6c792838ae
SyntaxPratyush <157193581+SyntaxPratyush@users.noreply.github.com>
2024-01-20 12:56:01 +0530

    Added Heading

--- mainprj.py
+++ mainprj.py
@@ -11,6 +11,11 @@ def home_page():
     home.geometry("360x640")
     home.title("Welcome to Eco-Touri")
     home.configure(bg="#1D3C37")
+    
+    headingimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Heading.png")
+    headinglabel = Label(home, image=headingimage)
+    headinglabel.grid(row=0, column=0, columnspan=4)
+
     home.mainloop()
 
 if __name__ == "__main__":
