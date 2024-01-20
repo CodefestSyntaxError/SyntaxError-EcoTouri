1c52420ff1d90f879cf40d5493a36314e45608eb
SyntaxPratyush <157193581+SyntaxPratyush@users.noreply.github.com>
2024-01-20 12:28:24 +0530

    Added Basic Geometry & Configurated UI

--- mainprj.py
+++ mainprj.py
@@ -6,4 +6,12 @@ import os
 import time
 from tkinter import messagebox
 
+def home_page():
+    home = tk.Tk()
+    home.geometry("360x640")
+    home.title("Welcome to Eco-Touri")
+    home.configure(bg="#1D3C37")
+    home.mainloop()
 
+if __name__ == "__main__":
+    home_page()
