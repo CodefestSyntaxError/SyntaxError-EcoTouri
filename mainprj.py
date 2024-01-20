8ac98ae46fd041aaeb18ee0e3e2265b4da192729
Anonymous-alph <97744086+Anonymous-alph@users.noreply.github.com>
2024-01-20 15:01:35 +0530

    Added Image Carousel

--- mainprj.py
+++ mainprj.py
@@ -7,6 +7,7 @@ import time
 from tkinter import messagebox
 
 def home_page():
+    global x
     home = tk.Tk()
     home.geometry("360x640")
     home.title("Welcome to Eco-Touri")
@@ -31,6 +32,33 @@ def home_page():
     reviewimg = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Reviewbtn.png")
     reviewbtn = Button(home, image=reviewimg, highlightthickness=0, borderwidth=0)
     reviewbtn.grid(row=1, column=3, columnspan=1)
+
+    car1 = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Car1.jpg")
+    car2 = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Car2.jpg")
+    car3 = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Car3.jpg")
+    car4 = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Car4.jpg")
+    car5 = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Car5.jpg")
+
+    l = Label(borderwidth=0, highlightthickness=0)
+    x = 1
+    def carousel():
+        global x
+        if x == 6: 
+            x = 1
+        if x == 1: 
+            l.config(image=car1) 
+        elif x == 2: 
+            l.config(image=car2) 
+        elif x == 3: 
+            l.config(image=car3)
+        elif x == 4: 
+            l.config(image=car4) 
+        elif x == 5: 
+            l.config(image=car5) 
+        x = x+1
+        l.after(3000, carousel)
+    carousel()
+    l.grid(row=3, column=0, columnspan=100, rowspan=100,pady=10) 
 
     home.mainloop()
 
