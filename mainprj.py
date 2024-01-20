e2d1c0f44d4b814ab5bd455e26f940bbc7b768e6
Anonymous-alph <97744086+Anonymous-alph@users.noreply.github.com>
2024-01-20 15:58:11 +0530

    Added login page

--- mainprj.py
+++ mainprj.py
@@ -6,6 +6,45 @@ import os
 import time
 from tkinter import messagebox
 
+def login_page():
+    login = tk.Tk()
+    login.geometry("360x640")
+    login.title("Welcome to Eco-Touri")
+    login.configure(bg="#1D3C37")
+    
+    headingimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Loginhead.png")
+    headinglabel = Label(login, image=headingimage, highlightthickness=0, borderwidth=0)
+    headinglabel.grid(row=0, column=0, columnspan=4)
+    
+    usernameimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Username.png")
+    username_label = tk.Label(login, image=usernameimage, highlightthickness=0, borderwidth=0)
+    username_label.grid(row=1,column=0,columnspan=4,pady=20)
+
+    username_entry = tk.Entry(login)
+    username_entry.grid(row=2,column=0,columnspan=4,pady=20)
+
+    passwordimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Password.png")
+    password_label = tk.Label(login, image=passwordimage, highlightthickness=0, borderwidth=0)
+    password_label.grid(row=3,column=0,columnspan=4, pady=20)
+
+    password_entry = tk.Entry(login, show="*") 
+    password_entry.grid(row=4,column=0,columnspan=4,pady=20)
+    
+    def validate_login():
+        userid = username_entry.get()
+        password = password_entry.get()
+
+        if userid == "admin" and password == "password":
+            messagebox.showinfo("Login Successful", "Welcome, Admin!")
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+    
+    confirmimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Confirmbtn.png")
+    confirm_button = tk.Button(login, image=confirmimage, command=validate_login, highlightthickness=0, borderwidth=0)
+    confirm_button.grid(row=5,column=0,columnspan=4,pady=20)
+    
+    login.mainloop()
+
 def home_page():
     global x
     home = tk.Tk()
@@ -13,6 +52,13 @@ def home_page():
     home.title("Welcome to Eco-Touri")
     home.configure(bg="#1D3C37")
     
+    def close_window():
+        home.destroy()
+        
+    def login_btn():
+        close_window()
+        login_page()
+
     headingimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Heading.png")
     headinglabel = Label(home, image=headingimage, highlightthickness=0, borderwidth=0)
     headinglabel.grid(row=0, column=0, columnspan=4)
@@ -58,7 +104,15 @@ def home_page():
         x = x+1
         l.after(3000, carousel)
     carousel()
-    l.grid(row=3, column=0, columnspan=100, rowspan=100,pady=10) 
+    l.grid(row=3, column=0, columnspan=4, pady=10) 
+    
+    descriptionimage = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Description.png")
+    descriptionlabel = Label(home, image=descriptionimage, highlightthickness=0, borderwidth=0)
+    descriptionlabel.grid(row=4, column=0, columnspan=4) 
+    
+    loginbtnimg = ImageTk.PhotoImage(file="C:\\Users\\admin\\Desktop\\Codefest\\Eco-Touri_Loginbtn.png")
+    loginbtn = Button(home, image=loginbtnimg, highlightthickness=0, borderwidth=0, command = login_btn)
+    loginbtn.grid(row=5, column=0, columnspan=4, pady=10)
 
     home.mainloop()
 
